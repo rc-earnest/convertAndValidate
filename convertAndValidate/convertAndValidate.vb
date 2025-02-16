e0d45bca3b6d3f19a070d3dc4791dc9fe92b867c
rc-earnest <rc.earnest04@gmail.com>
2025-02-16 16:16:37 -0700

    changed name and added the base code

---
+++ convertAndValidate/convertAndValidate.vb
@@ -0,0 +1,53 @@
+﻿'Rudy Earnest
+'RCET 2265
+'Spring 2025
+'ConvertAndValidate
+'
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+
+Module convertAndValidate
+
+    Sub Main()
+        Dim aValidNumber As Integer
+        Dim userResponse As String
+        Do
+
+            Console.WriteLine($"Enter a number:")
+
+            userResponse = Console.ReadLine()
+
+            If ConversionValid(userResponse, aValidNumber) = True Then
+
+                Console.WriteLine($"{userResponse} converted successfully to {aValidNumber}!")
+
+            Else
+
+                Console.WriteLine($"Oops, {userResponse} does not seem to be a number")
+
+            End If
+
+        Loop
+
+    End Sub
+
+    'make this work
+
+
+    Function ConversionValid(ByVal convertThisString As String, ByVal toThisInteger As Integer) As Boolean
+
+        Dim status As Boolean
+
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+        Return status
+
+    End Function
+
+End Module
